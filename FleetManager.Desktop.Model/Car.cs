dc66323f4eac24f1fc5c5cb6bc53a8e7647d46bb
Lars Nysom <LANY@ucn.dk>
2021-10-18 00:06:14 +0200

    Add project files.

---
+++ FleetManager.Desktop.Model/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FleetManager.Desktop.Model
+{
+    public class Car
+    {
+        public int? Id { get; set; }
+        public string Brand { get; set; }
+        public int? Mileage { get; set; }
+        public Location Location { get; set; }
+    }
+}
---
+++ FleetManager.Desktop/Presenter/CarPresenter.cs
@@ -0,0 +1,54 @@
+﻿using FleetManager.Desktop.Data;
+using FleetManager.Desktop.Model;
+using System.Collections.Generic;
+
+namespace FleetManager.Desktop.Presenter
+{
+    public class CarPresenter
+    {
+        private readonly IDao<Car> _carData;
+        private readonly IDao<Location> _locationData;        
+
+        public CarPresenter()
+        {
+            // TODO: (Step 2) change the call to the dao factory so it recieves the data context
+            // for the WebAPI and thus returns a dao that calls that instead of the test data
+            _carData = DaoFactory.Create<Car>(MemoryDataContext.Instance);
+            _locationData = DaoFactory.Create<Location>(MemoryDataContext.Instance);
+        }
+
+        public IEnumerable<CarModel> GetAllCars()
+        {
+            foreach (Car car in _carData.Read())
+            {
+                yield return car.Map();
+            }
+        }
+
+        public IEnumerable<LocationModel> GetAllLocations()
+        {
+            foreach (Location location in _locationData.Read())
+            {
+                yield return location.Map();
+            }
+        }
+
+        public CarModel SaveCar(CarModel car)
+        {
+            if (car.Id.HasValue) // update car
+            {
+                _carData.Update(car.Map());
+            }
+            else // create car
+            {
+                _carData.Create(car.Map());
+            }
+            return car;
+        }
+
+        public bool DeleteCar(CarModel car)
+        {
+            return _carData.Delete(car.Map());
+        }
+    }
+}
